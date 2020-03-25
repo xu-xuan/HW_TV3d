321adfd6ba01e8c820338e5a7f7a5cdd9f23e1b7
wenj18 <630560526@qq.com>
2020-03-25 23:18:44 +0800

    final TV3d is OK

--- TV3d2.py
+++ TV3d2.py
@@ -1,77 +1,57 @@
 import numpy as np
 
-def bdx(P,m):
-  return P-P[[0]+list(range(m-1)),:,:]
-
-def bdy(P,n):
-  return P-P[:,[0]+list(range(n-1)),:]    
-
-def bdz(P,l):
-  return P-P[:,:,[0]+list(range(l-1))]	
 	
 def worker(In, J, dt, lam):
   ep = 1e-4
   m,n,l = J.shape
-  DfJx=J[list(range(1,m))+[m-1],:,:]-J
-  DfJy=J[:,list(range(1,n))+[n-1],:]-J
-  DfJz=J[:,:,list(range(1,l))+[l-1]]-J
-
-  TempDJ=(ep+DfJx*DfJx+DfJy*DfJy+DfJz*DfJz)**(1/2)
-  DivJx=DfJx/TempDJ;
-  DivJy=DfJy/TempDJ;
-  DivJz=DfJz/TempDJ;
-	
-  Div=bdx(DivJx,m)+bdy(DivJy,n)+bdz(DivJz,l)
-          
-  J += dt * Div -dt*lam*(J-In)
+  for i in range(8):
+    start = int(i*m/8-1)
+    end = int((i+1)*m/8+1)
+    #print(start,end)	
+    if start < 0:
+      start = start +1
+      DfJx=J[(start+1):(end+1),:,:]-J[start:end,:,:]	  
+    elif end > m:
+      end = end-1
+      DfJx=J[list(range(start+1,end))+[end-1],:,:]-J[start:end,:,:]
+    else:
+      DfJx=J[(start+1):(end+1),:,:]-J[start:end,:,:]
+    DfJy=J[start:end,list(range(1,n))+[n-1],:]-J[start:end,:,:]
+    DfJz=J[start:end,:,list(range(1,l))+[l-1]]-J[start:end,:,:]
+    TempDJ=(ep+DfJx*DfJx+DfJy*DfJy+DfJz*DfJz)**(1/2)
+    DivJx=DfJx/TempDJ;
+    DivJy=DfJy/TempDJ;
+    DivJz=DfJz/TempDJ;  
+    del TempDJ
+    mi,ni,li = DivJx.shape
+    if start==0:
+      div = DivJx[0:(mi-1),:,:]-DivJx[[0]+list(range(mi-2)),:,:] \
+        + DivJy[0:(mi-1),:,:]-DivJy[0:(mi-1),[0]+list(range(ni-1)),:]\
+        + DivJz[0:(mi-1),:,:]-DivJz[0:(mi-1),:,[0]+list(range(li-1))]
+      J[start:(end-1),:,:] += dt * div -dt*lam*(J[start:(end-1),:,:]-In[start:(end-1),:,:])  
+    elif end == m:
+      mi = mi + 1
+      end = end + 1	  
+     # print(mi)
+      div = DivJx[1:(mi-1),:,:]-DivJx[0:(mi-2),:,:] \
+        + DivJy[1:(mi-1),:,:]-DivJy[1:(mi-1),[0]+list(range(ni-1)),:]\
+        + DivJz[1:(mi-1),:,:]-DivJz[1:(mi-1),:,[0]+list(range(li-1))]
+      J[(start+1):(end-1),:,:] += dt * div -dt*lam*(J[(start+1):(end-1),:,:]-In[(start+1):(end-1),:,:])  
+    else:
+      div = DivJx[1:(mi-1),:,:]-DivJx[0:(mi-2),:,:] \
+        + DivJy[1:(mi-1),:,:]-DivJy[1:(mi-1),[0]+list(range(ni-1)),:]\
+        + DivJz[1:(mi-1),:,:]-DivJz[1:(mi-1),:,[0]+list(range(li-1))]
+      J[(start+1):(end-1),:,:] += dt * div -dt*lam*(J[(start+1):(end-1),:,:]-In[(start+1):(end-1),:,:])  	
   return J
 
-def rank2coord(rank, ncoord):
-  # warning： ncoord == [2,2,2]
-  c = '{:03b}'.format(int(rank))
-  coord = [eval(c[i]) for i in range(3)]
-  return coord
-
-def coord2rank(coord, ncoord):
-  # warning： ncoord[i] == 2
-  dim = len(ncoord)
-  rank = 0
-  for i in range(dim):
-    rank += coord[i]*(2**(dim-i-1))
-  return rank
-	
-
-def Send4coord(data, destcoord, tag, ncoord):
-  if destcoord is None:
-    return None
-  else:
-    sendbuf = data.copy()
-    destrank = coord2rank(destcoord,ncoord)
-    #print(destrank)
-    comm.Send(sendbuf, dest=destrank, tag=tag)
-    del sendbuf
-    return 1
-  
-def Recv4coord(data, sourcecoord, tag, ncoord):
-  if sourcecoord is None:
-    return None
-  else:
-    recbuf = np.empty(data.shape, dtype=data.dtype)
-    sourcerank = coord2rank(sourcecoord,ncoord)
-    #print(sourcerank)
-    comm.Recv(recbuf, source=sourcerank, tag=tag)
-    return recbuf	
 	
 if __name__ == '__main__':
   from mpi4py import MPI
   import matplotlib.pyplot as plt # plt 用于显示图片
-  T, dt, lam = 100, 0.1, 0.01 
+  T, dt, lam = 200, 0.1, 0.01 
   comm = MPI.COMM_WORLD
   size = comm.Get_size()
   rank = comm.Get_rank()
-  ncoord = [1,1,2]
-  coord = rank2coord(rank, ncoord)
-  
   # np = 2, ncoord = [2,1,1]
   # The initial distribution
   if rank == 0:
@@ -82,22 +62,22 @@ if __name__ == '__main__':
 	# Adding Gaussian noise
     nmean, nsigma = 0.0, 12.0
     nimg = np.random.normal(nmean,nsigma,(nx,ny,nz)) + img
-    plt.figure()
-    plt.imshow(img[:,100,:],"gray")
-    plt.savefig('./img.png')
-    plt.figure()
-    plt.imshow(nimg[:,100,:],"gray")
-    plt.savefig('./nimg.png')	
+    # plt.figure()
+    # plt.imshow(img[:,100,:],"gray")
+    # plt.savefig('./img.png')
+    # plt.figure()
+    # plt.imshow(nimg[:,100,:],"gray")
+    # plt.savefig('./nimg.png')	
     del img
     nimgsile = nimg[:,:,0:101]
     sendbuf = nimg[:,:,99:200].copy()
-    plt.figure()
-    plt.imshow(sendbuf[:,100,:],"gray")
-    plt.savefig('./sendbuf.png')	
+    # plt.figure()
+    # plt.imshow(sendbuf[:,100,:],"gray")
+    # plt.savefig('./sendbuf.png')	
     comm.Send(sendbuf, dest=1, tag=11)
-    plt.figure()
-    plt.imshow(nimgsile[:,100,:],"gray")
-    plt.savefig('./nimgsile0.png')	
+    # plt.figure()
+    # plt.imshow(nimgsile[:,100,:],"gray")
+    # plt.savefig('./nimgsile0.png')	
     del nimg, sendbuf
   else: 
     nimgsile = np.empty([200,200,101],dtype=np.float64) 
@@ -106,18 +86,6 @@ if __name__ == '__main__':
     plt.imshow(nimgsile[:,100,:],"gray")
     plt.savefig('./nimgsile1.png')	
 
-  # if coord[2]>0:
-    # frontcoord = coord.copy()
-    # frontcoord[2] = coord[2]-1
-  # else: 
-    # frontcoord = None
-	
-  # if coord[2]<ncoord[2]-1:
-    # backcoord = coord.copy()
-    # backcoord[2] = coord[2]+1
-  # else:
-    # backcoord = None 
-  #print(frontcoord,backcoord, 'of', coord, rank)
   # Iterative
   J = nimgsile.copy()
   n0,n1,n2 = J.shape  
@@ -128,24 +96,28 @@ if __name__ == '__main__':
     #print('good work', rank )	
     if rank == 0:
       sendbuf = J[:,:,n2-2].copy()
-      recbuf = np.empty(J[:,:,n2-1].shape, dtype=J[:,:,n2-1].dtype)
       #print('begin send of', rank )	  
       comm.Send(sendbuf, dest=1, tag=100)
+      del sendbuf
+      recbuf = np.empty(J[:,:,n2-1].shape, dtype=J[:,:,n2-1].dtype)	  
       #print('goodsend of', rank )
       comm.Recv(recbuf, source=1, tag=110)	
       #print('goodrecv of', rank )
       J[:,:,n2-1] = recbuf
+      del recbuf
     else:
-      sendbuf = J[:,:,1].copy()
       recbuf = np.empty(J[:,:,n2-1].shape, dtype=J[:,:,n2-1].dtype)
       #print('begin send of', rank )		  
       comm.Recv(recbuf, source=0, tag=100)
+      J[:,:,0] = recbuf
+      del recbuf	  
       #print('goodsend of', rank )	  
+      sendbuf = J[:,:,1].copy()	  
       comm.Send(sendbuf, dest=0, tag=110)
+      del sendbuf
       #print('goodsend of', rank )	  
-      J[:,:,0] = recbuf	  	  
-	
-   
+ 	  
+	   
   if rank ==0:
 	# 接受数据
     deimg = np.empty([nx,ny,nz], dtype = np.float64)
@@ -162,12 +134,7 @@ if __name__ == '__main__':
     comm.Send(sendbuf, dest=0, tag=20)  
     del sendbuf
 	
-	
-	
-	
-	
-	
-	
+		
 	
 	
 	
